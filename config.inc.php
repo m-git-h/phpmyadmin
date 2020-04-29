0a73d88dd7daf4fbcfacf3a77732a163f81d99ba
supak111 <samtos@sbcglobal.net>
2020-04-29 00:35:09 -0500

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -37,32 +37,32 @@ $cfg['Servers'][$i]['AllowNoPassword'] = false;
  */
 
 /* User used to manipulate with storage */
-// $cfg['Servers'][$i]['controlhost'] = '';
-// $cfg['Servers'][$i]['controlport'] = '';
+$cfg['Servers'][$i]['controlhost'] = '';
+$cfg['Servers'][$i]['controlport'] = '';
 $cfg['Servers'][$i]['controluser'] = 'pma';
 $cfg['Servers'][$i]['controlpass'] = 'pmapass';
 
 /* Storage database and tables */
-// $cfg['Servers'][$i]['pmadb'] = 'phpmyadmin';
-// $cfg['Servers'][$i]['bookmarktable'] = 'pma__bookmark';
-// $cfg['Servers'][$i]['relation'] = 'pma__relation';
-// $cfg['Servers'][$i]['table_info'] = 'pma__table_info';
-// $cfg['Servers'][$i]['table_coords'] = 'pma__table_coords';
-// $cfg['Servers'][$i]['pdf_pages'] = 'pma__pdf_pages';
-// $cfg['Servers'][$i]['column_info'] = 'pma__column_info';
-// $cfg['Servers'][$i]['history'] = 'pma__history';
-// $cfg['Servers'][$i]['table_uiprefs'] = 'pma__table_uiprefs';
-// $cfg['Servers'][$i]['tracking'] = 'pma__tracking';
-// $cfg['Servers'][$i]['userconfig'] = 'pma__userconfig';
-// $cfg['Servers'][$i]['recent'] = 'pma__recent';
-// $cfg['Servers'][$i]['favorite'] = 'pma__favorite';
-// $cfg['Servers'][$i]['users'] = 'pma__users';
-// $cfg['Servers'][$i]['usergroups'] = 'pma__usergroups';
-// $cfg['Servers'][$i]['navigationhiding'] = 'pma__navigationhiding';
-// $cfg['Servers'][$i]['savedsearches'] = 'pma__savedsearches';
-// $cfg['Servers'][$i]['central_columns'] = 'pma__central_columns';
-// $cfg['Servers'][$i]['designer_settings'] = 'pma__designer_settings';
-// $cfg['Servers'][$i]['export_templates'] = 'pma__export_templates';
+$cfg['Servers'][$i]['pmadb'] = 'phpmyadmin';
+$cfg['Servers'][$i]['bookmarktable'] = 'pma__bookmark';
+$cfg['Servers'][$i]['relation'] = 'pma__relation';
+$cfg['Servers'][$i]['table_info'] = 'pma__table_info';
+$cfg['Servers'][$i]['table_coords'] = 'pma__table_coords';
+$cfg['Servers'][$i]['pdf_pages'] = 'pma__pdf_pages';
+$cfg['Servers'][$i]['column_info'] = 'pma__column_info';
+$cfg['Servers'][$i]['history'] = 'pma__history';
+$cfg['Servers'][$i]['table_uiprefs'] = 'pma__table_uiprefs';
+$cfg['Servers'][$i]['tracking'] = 'pma__tracking';
+$cfg['Servers'][$i]['userconfig'] = 'pma__userconfig';
+$cfg['Servers'][$i]['recent'] = 'pma__recent';
+$cfg['Servers'][$i]['favorite'] = 'pma__favorite';
+$cfg['Servers'][$i]['users'] = 'pma__users';
+$cfg['Servers'][$i]['usergroups'] = 'pma__usergroups';
+$cfg['Servers'][$i]['navigationhiding'] = 'pma__navigationhiding';
+$cfg['Servers'][$i]['savedsearches'] = 'pma__savedsearches';
+$cfg['Servers'][$i]['central_columns'] = 'pma__central_columns';
+$cfg['Servers'][$i]['designer_settings'] = 'pma__designer_settings';
+$cfg['Servers'][$i]['export_templates'] = 'pma__export_templates';
 
 /**
  * End of servers configuration
@@ -79,14 +79,14 @@ $cfg['SaveDir'] = '';
  * action segment. Value can be either of 'icons', 'text' or 'both'.
  * default = 'both'
  */
-//$cfg['RowActionType'] = 'icons';
+$cfg['RowActionType'] = 'both';
 
 /**
  * Defines whether a user should be displayed a "show all (records)"
  * button in browse mode or not.
  * default = false
  */
-//$cfg['ShowAll'] = true;
+$cfg['ShowAll'] = false;
 
 /**
  * Number of rows displayed when browsing a result set. If the result
@@ -94,7 +94,7 @@ $cfg['SaveDir'] = '';
  * Possible values: 25, 50, 100, 250, 500
  * default = 25
  */
-//$cfg['MaxRows'] = 50;
+$cfg['MaxRows'] = 100;
 
 /**
  * Disallow editing of binary fields
@@ -105,7 +105,7 @@ $cfg['SaveDir'] = '';
  *   'all'    disallow editing
  * default = 'blob'
  */
-//$cfg['ProtectBinary'] = false;
+$cfg['ProtectBinary'] = 'blob';
 
 /**
  * Default language to use, if not browser-defined or user-defined
@@ -113,7 +113,7 @@ $cfg['SaveDir'] = '';
  * uncomment the desired line:
  * default = 'en'
  */
-//$cfg['DefaultLang'] = 'en';
+$cfg['DefaultLang'] = 'en';
 //$cfg['DefaultLang'] = 'de';
 
 /**
@@ -121,7 +121,7 @@ $cfg['SaveDir'] = '';
  * (a value larger than 1 results in some information being hidden)
  * default = 1
  */
-//$cfg['PropertiesNumColumns'] = 2;
+$cfg['PropertiesNumColumns'] = 1;
 
 /**
  * Set to true if you want DB-based query history.If false, this utilizes
@@ -130,13 +130,13 @@ $cfg['SaveDir'] = '';
  * This requires configuration storage enabled, see above.
  * default = false
  */
-//$cfg['QueryHistoryDB'] = true;
+$cfg['QueryHistoryDB'] = false;
 
 /**
  * When using DB-based query history, how many entries should be kept?
  * default = 25
  */
-//$cfg['QueryHistoryMax'] = 100;
+$cfg['QueryHistoryMax'] = 100;
 
 /**
  * Whether or not to query the user before sending the error report to
@@ -146,7 +146,7 @@ $cfg['SaveDir'] = '';
  * ('ask' | 'always' | 'never')
  * default = 'ask'
  */
-//$cfg['SendErrorReports'] = 'always';
+$cfg['SendErrorReports'] = 'ask';
 
 /**
  * You can find more configuration options in the documentation
