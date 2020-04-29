447eccb2308b3d1645926ba1a94572aaea620437
supak111 <samtos@sbcglobal.net>
2020-04-29 01:14:32 -0500

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -14,7 +14,7 @@
  * This is needed for cookie based authentication to encrypt password in
  * cookie. Needs to be 32 chars long.
  */
-$cfg['blowfish_secret'] = $_ENV['PHPMYADMIN_BLOWFISH_SECRET']; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
+$cfg['blowfish_secret'] = '6s=mYAN0kU20}ul0,=4VCiLm0VdU/G9D'; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
 
 /**
  * Servers configuration
